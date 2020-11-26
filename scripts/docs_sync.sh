3597de86d2bb4d080d0baf8897f27db0df5507c7
carla <kirkcohenc@gmail.com>
2020-11-26 11:16:36 +0200

    scripts: add script to diff and commit docs

---
+++ scripts/docs_sync.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [ $# -ne 3 ];
+    then echo "target directory, upstream and downstream repo required"
+    exit 1
+fi
+
+# We will get our files from the upstream/path provided.
+upstreamDir="$2"/"$1"
+
+# We will copy these files to downstream/upstream/path
+downstreamPath="$2"/"$1"
+downstreamDir="$3"/"$downstreamPath"
+
+# Create our downstream dir in case it does not yet exist, because we cannot
+# diff a directory that does not exist.
+mkdir -p "$downstreamDir"
+
+# Check whether there is any difference between our upstream docs folder and our
+# current copy, exiting early if there is no difference.
+diff=$(git diff "$upstreamDir" "$downstreamDir" )
+if [ -z "$diff" ];
+    then
+      echo "No difference in docs folder"
+      exit 1
+
+    else
+      echo "Docs differ, syncing documents"
+fi
+
+# Copy everything from upstream to downstream, replacing what's there.
+cp -rf "$upstreamDir"/* "$downstreamDir" || exit 1
+
+# Now we want to commit these changes to our downstream repo.
+cd "$3" || exit 1
+
+git add .
+git commit -m "docsync: $downstreamPath at $(date)"
+
